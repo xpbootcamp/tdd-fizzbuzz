78bb9b0ef9ba43dda3edf3e579cbcc012910921a
nalansitan <nalansitan@gmail.com>
2020-05-10 14:20:22 +0800

    rule2: say divided_by_3_5_7

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,16 @@ package cn.xpbootcamp.fizzbuzz;
 
 public class FizzBuzz {
     public String SayNumbers(int inputNum) {
-        return String.valueOf(inputNum);
+        String result  = String.valueOf(inputNum);
+        if (inputNum % 3 == 0) {
+            result = "Fizz";
+        }
+        if (inputNum % 5 == 0) {
+            result = "Buzz";
+        }
+        if (inputNum % 7 == 0) {
+            result = "Whizz";
+        }
+        return result;
     }
 }
--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -6,8 +6,21 @@ import org.junit.Test;
 public class FizzBuzzTest {
     @Test
     public void when_give_a_num_then_say_num(){
-        int inputNum = 1;
         FizzBuzz fb = new FizzBuzz();
-        Assert.assertEquals("1",fb.SayNumbers(inputNum));
+        Assert.assertEquals("1",fb.SayNumbers(1));
+        Assert.assertEquals("2",fb.SayNumbers(2));
+        Assert.assertEquals("4",fb.SayNumbers(4));
     }
+
+    @Test
+    public void when_give_a_num_divided_by_3_5_7(){
+        FizzBuzz fb = new FizzBuzz();
+        Assert.assertEquals("Fizz",fb.SayNumbers(3));
+        Assert.assertEquals("Fizz",fb.SayNumbers(6));
+        Assert.assertEquals("Buzz",fb.SayNumbers(5));
+        Assert.assertEquals("Buzz",fb.SayNumbers(10));
+        Assert.assertEquals("Whizz",fb.SayNumbers(7));
+        Assert.assertEquals("Whizz",fb.SayNumbers(14));
+    }
+
 }
